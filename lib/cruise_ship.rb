fdd7108199f183f8f3e3a400342101fd5ca997ca
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 19:39:19 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -14,8 +14,7 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == suite_a: && name.start_with?("A")
       winner = name
-    
   end
 end
 
-select_winner(passengers)
+
