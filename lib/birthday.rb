05e90dc003b70da1bf401316d4177a3bf82c6506
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 19:44:04 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,6 +8,6 @@ def happy_birthday(birthday_kids)
   # add your code snippet here!
 
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{:name}! You are now 9 years old!"
+    puts "Happy Birthday #{:name}! You are now #{age}"
 
 end
